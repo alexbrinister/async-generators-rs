3fd2b4666636ea19cf0f8af560ce08b3813b6fb1
Alex Brinister <IrVQ55Gw9TZ7BW8e@tuta.io>
2025-06-15 09:05:32 -0400

    added prototype for walking bit data
    Signed-off-by: Alex Brinister <IrVQ55Gw9TZ7BW8e@tuta.io>

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,18 @@
+//! Simple add function
+///
+/// Adds two numbers together and produces the sum.
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
+}
+
+/// Create a vector of walking bits.
+///
+/// # Arguments
+///
+/// * `length` - Length of the output data pattern vector.
+/// * `walking_ones` - makes output a walking 1s pattern instead of the default walking 0s.
+pub fn make_walking_bit_data(length: usize, walking_ones: bool) -> Vec<u32> {
+    todo!()
 }
 
 #[cfg(test)]
